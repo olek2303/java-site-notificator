dc14b76c406593c09d11484a76ed99d75c2a30df
olek2303 <olek230303@gmail.com>
2023-02-24 11:59:01 +0100

    not working one site, and the function with selentium does not work

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,20 @@
 import org.apache.commons.codec.digest.*;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
 
 import java.io.*;
 import java.net.*;
+import java.time.Duration;
 import java.util.*;
 
 public class Main {
     static Map<String, String> checkSumDB = new HashMap<String, String>();
+
     public static void main(String []args) throws IOException, InterruptedException {
         while (true) {
-            String url = "https://wiadomosci.onet.pl/";
+            String url = "https://www.otomoto.pl/osobowe/seg-cabrio--seg-city-car--seg-compact--seg-coupe--seg-sedan/od-2012?search%5Bfilter_float_mileage%3Ato%5D=150000&search%5Bfilter_float_engine_power%3Afrom%5D=100&search%5Bfilter_float_price%3Ato%5D=40000&search%5Bfilter_enum_damaged%5D=0&search%5Border%5D=created_at_first%3Adesc&search%5Badvanced_search_expanded%5D=true";
             // query last checksum from map
             String lastChecksum = checkSumDB.get(url);
             // get current checksum using static utility method
@@ -22,6 +25,7 @@ public class Main {
                 // persist this checksum to map
                 checkSumDB.put(url, currentChecksum);
                 System.out.println("something in the content have changed...");
+                String text = firstElementFind(url);
                 // send email you can check: http://www.javacommerce.com/displaypage.jsp?name=javamail.sql&id=18274
             }
             Thread.sleep(10000);
@@ -36,10 +40,20 @@ public class Main {
         return DigestUtils.sha256Hex(huc.getInputStream());
     }
 
-    private static String firstElementFind() {
+    public static String firstElementFind(String spec) {
         WebDriver driver = new ChromeDriver();
-        driver.get("https://wiadomosci.onet.pl/");
-
+        String text = "";
+        try {
+            driver.get(spec);
+            driver.manage().timeouts().implicitlyWait(Duration.ofMillis(10000));
+            WebElement textBoxe = driver.findElement(By.className("ooa-7wotau")); // name of the action box on otomoto
+            textBoxe = driver.findElement(By.className("ooa-9tzypk"));
+            textBoxe = driver.findElement(By.className("e1p19lg720"));
+            text = textBoxe.getText();
+            System.out.println("To jest tekst poszukiwanego elementu : " + text);
+        } finally {
+            driver.quit();
+        }
+        return text;
     }
-
 }
